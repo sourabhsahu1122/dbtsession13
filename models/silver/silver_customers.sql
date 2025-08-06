37580a563f719de6d596d9c070f89eb7b9a7e10e
sourabh sahu <deepikasahu181989@gmail.com>
2025-08-06 08:03:59 +0530

    this is my second commit

--- models/silver/silver_customers.sql
+++ models/silver/silver_customers.sql
@@ -11,4 +11,10 @@ WITH CTE_Customer AS (
         * from SESSION12.BRONZE.raw_customers
 )
 
-SELECT * from CTE_Customer
+SELECT customer_id,
+initcap(name) as name,
+age,
+initcap(city) as city,
+CASE WHEN age>18 THEN 'Adult'
+     ELSE 'Minor' END as age_group
+ from CTE_Customer
